86a368e5f89e51d8817d6c4c61d6f0c4c287ee12
tidvn <tiendung0325@gmail.com>
2024-11-26 06:06:40 +0700

    refactor: improve layout and styling in CollectionPage and StoragePage components

--- src/app/(app)/dashboard/utilities/collection/page.tsx
+++ src/app/(app)/dashboard/utilities/collection/page.tsx
@@ -5,25 +5,21 @@ import { useCollectionContext } from "@/contexts/collection";
 export default function CollectionPage() {
   const { listCollection } = useCollectionContext();
   return (
-    <div className="py-8 px-10 m-auto flex flex-col max-md:px-0">
-      <div className="rounded-xl p-6 bg-section shadow-md flex-wrap gap-3 space-y-5">
-        <h1 className="text-2xl font-semibold leading-7">
-          Collection Metadata
-        </h1>
-        <div className="mt-5">
-          <div className="flex flex-wrap items-center justify-between gap-2 rounded-lg p-2">
-            <div />
-            <div className="flex items-center space-x-2">
-              <CreateCollectionButton />
-            </div>
+    <div className="mt-5 rounded-lg bg-section p-4">
+      <h1 className="text-2xl font-semibold leading-7">Collection Metadata</h1>
+      <div className="mt-5">
+        <div className="flex flex-wrap items-center justify-between gap-2 rounded-lg p-2">
+          <div />
+          <div className="flex items-center space-x-2">
+            <CreateCollectionButton />
           </div>
-          <div className="h-full w-full space-y-4 rounded-lg p-4">
-            <div className="overflow-x-auto">
-              <div className="md:grid-col-2 grid grid-cols-1 gap-4 lg:grid-cols-3 xl:grid-cols-4">
-                {listCollection.map((collection, index) => (
-                  <FolderCard collection={collection} key={index} />
-                ))}
-              </div>
+        </div>
+        <div className="h-full w-full space-y-4 rounded-lg p-4">
+          <div className="overflow-x-auto">
+            <div className="md:grid-col-2 grid grid-cols-1 gap-4 lg:grid-cols-3 xl:grid-cols-4">
+              {listCollection.map((collection, index) => (
+                <FolderCard collection={collection} key={index} />
+              ))}
             </div>
           </div>
         </div>
