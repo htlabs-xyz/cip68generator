0dfc4dd2c6d4a34802e9b1af1bc85503de89f75d
Nguyễn Khánh <108068667+independenceee@users.noreply.github.com>
2024-11-26 12:44:05 +0700

    develop: add animation to landing page

--- src/app/(landing)/_components/founder-section.tsx
+++ src/app/(landing)/_components/founder-section.tsx
@@ -1,11 +1,13 @@
 "use client";
-import { cn } from "@/utils";
+
 import Image, { StaticImageData } from "next/image";
 import Link from "next/link";
 import {
   FaTwitter as TwitterIcon,
   FaLinkedinIn as LinkedinIcon,
 } from "react-icons/fa";
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 type Props = {
   id?: number;
@@ -17,6 +19,7 @@ type Props = {
   twitter: string;
   linkedin: string;
   description: string;
+  index: number;
 };
 const Founder = function ({
   avatar,
@@ -26,35 +29,33 @@ const Founder = function ({
   twitter,
   linkedin,
   description,
+  index,
 }: Props) {
+  const { ref, inView } = useInView({
+    threshold: 0.2,
+    triggerOnce: true,
+  });
   return (
-    <div
-      className={cn(
-        "flex flex-col items-center gap-5 rounded-2xl bg-slate-900 p-7 transition-all duration-300 ease-in-out",
-      )}
+    <motion.div
+      ref={ref}
+      initial={{ y: 100, scale: 0.5, opacity: 0 }}
+      animate={inView ? { y: 0, scale: 1, opacity: 1 } : {}}
+      transition={{
+        duration: 0.5 + index * 0.5,
+        ease: "easeOut",
+      }}
+      className="flex flex-col items-center gap-5 rounded-2xl bg-slate-900 p-7 transition-all duration-300 ease-in-out"
     >
-      <div
-        className={cn(
-          "bg-[rgba(0, 0, 0, 0.08)] relative h-[150px] w-[150px] overflow-hidden rounded-full transition-all duration-500",
-        )}
-      >
+      <div className="bg-[rgba(0, 0, 0, 0.08)] relative h-[150px] w-[150px] overflow-hidden rounded-full transition-all duration-500">
         <Image
-          className={cn(
-            "absolute inset-0 h-full w-full object-cover text-transparent transition-all duration-500 ease-in-out hover:scale-150",
-          )}
+          className="absolute inset-0 h-full w-full object-cover text-transparent transition-all duration-500 ease-in-out hover:scale-150"
           src={avatar}
           alt="Avatar"
         />
-        <div
-          className={cn(
-            "absolute bottom-0 left-0 right-0 flex h-12 justify-center gap-4 transition-all duration-500",
-          )}
-        >
+        <div className="absolute bottom-0 left-0 right-0 flex h-12 justify-center gap-4 transition-all duration-500">
           {twitter && (
             <Link
-              className={cn(
-                "relative inline-flex h-7 w-7 items-center justify-center rounded-full bg-[rgba(0,0,0,0.4)] text-[16px] text-white",
-              )}
+              className="relative inline-flex h-7 w-7 items-center justify-center rounded-full bg-[rgba(0,0,0,0.4)] text-[16px] text-white"
               target="_blank"
               href={twitter}
             >
@@ -63,9 +64,7 @@ const Founder = function ({
           )}
           {linkedin && (
             <Link
-              className={cn(
-                "relative inline-flex h-7 w-7 items-center justify-center rounded-full bg-[rgba(0,0,0,0.4)] text-[16px] text-white",
-              )}
+              className="relative inline-flex h-7 w-7 items-center justify-center rounded-full bg-[rgba(0,0,0,0.4)] text-[16px] text-white"
               target="_blank"
               href={linkedin}
             >
@@ -74,30 +73,18 @@ const Founder = function ({
           )}
         </div>
       </div>
-      <div className={cn("flex flex-col gap-[10px]")}>
-        <div
-          className={cn(
-            "text-center text-[16px] font-medium leading-[120%] text-white",
-          )}
-        >
+      <div className="flex flex-col gap-[10px]">
+        <div className="text-center text-[16px] font-medium leading-[120%] text-white">
           {firstName + " " + lastName}
         </div>
-        <div
-          className={cn(
-            "text-center text-[13px] font-medium leading-[120%] text-white",
-          )}
-        >
+        <div className="text-center text-[13px] font-medium leading-[120%] text-white">
           {role}
         </div>
-        <div
-          className={cn(
-            "text-center text-[10px] font-normal leading-[120%] text-white",
-          )}
-        >
+        <div className="text-center text-[10px] font-normal leading-[120%] text-white">
           {description}
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
