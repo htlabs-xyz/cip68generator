26012dc9a6ccca7fe32202d4ceff7c870fd18940
tidvn <tiendung0325@gmail.com>
2024-11-29 01:59:11 +0700

    refactor: remove unused Layout component from collection utilities

--- src/app/(app)/dashboard/utilities/collection/layout.tsx
+++
@@ -1,6 +0,0 @@
-import { PropsWithChildren } from "react";
-import CollectionProvider from "@/contexts/collection";
-
-export default function Layout({ children }: Readonly<PropsWithChildren>) {
-  return <CollectionProvider>{children}</CollectionProvider>;
-}
