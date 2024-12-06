455fd2a823239709a459fd57e0238d578b39b87e
tidvn <tiendung0325@gmail.com>
2024-12-06 06:38:15 +0000

    fix: update DashboardLayout for improved mobile handling and desktop visibility

--- src/app/(app)/dashboard/layout.tsx
+++ src/app/(app)/dashboard/layout.tsx
@@ -19,13 +19,13 @@ export default function DashboardLayout({ children }: Readonly<PropsWithChildren
 
   return (
     <>
-      <div className="hidden md:flex flex-col items-center justify-center h-screen bg-gray-900">
+      <div className="lg:hidden h-screen flex items-center justify-center">
         <div className="text-white text-center py-6 px-8 max-w-md w-full">
           <h2 className="text-2xl font-semibold mb-4">Important Notice!</h2>
           <p className="text-lg">The application is not optimized for mobile devices yet. Please use a desktop for the best experience.</p>
         </div>
       </div>
-      <div className="md:hidden flex h-screen bg-gray-900 items-center justify-center">
+      <div className="hidden lg:block">
         <DesktopDashboardLayout>{children}</DesktopDashboardLayout>
       </div>
     </>
