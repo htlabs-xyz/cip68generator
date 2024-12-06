1f64a389986160364a5a023816dba3c1da1e726e
tidvn <tiendung0325@gmail.com>
2024-12-06 06:30:44 +0000

    fix: update DashboardLayout to improve mobile experience and enhance desktop layout visibility

--- src/app/(app)/dashboard/layout.tsx
+++ src/app/(app)/dashboard/layout.tsx
@@ -2,13 +2,11 @@
 
 import Loading from "@/app/(loading)/loading";
 import DesktopDashboardLayout from "@/components/layouts/desktop-dashboard";
-import useWindowSize from "@/hooks/use-window-size";
 import { useSession } from "next-auth/react";
 import { PropsWithChildren } from "react";
 import { redirect } from "next/navigation";
-export default function DashboardLayout({ children }: Readonly<PropsWithChildren>) {
-  const isMobile: boolean = useWindowSize();
 
+export default function DashboardLayout({ children }: Readonly<PropsWithChildren>) {
   const session = useSession();
 
   if (session.status === "loading") {
@@ -19,14 +17,17 @@ export default function DashboardLayout({ children }: Readonly<PropsWithChildren
     redirect("/login");
   }
 
-  if (isMobile)
-    return (
-      <div className="h-screen bg-gray-900 flex items-center justify-center">
-        <div className=" text-white text-center py-6 px-8  max-w-md w-full">
+  return (
+    <>
+      <div className="hidden md:flex flex-col items-center justify-center h-screen bg-gray-900">
+        <div className="text-white text-center py-6 px-8 max-w-md w-full">
           <h2 className="text-2xl font-semibold mb-4">Important Notice!</h2>
           <p className="text-lg">The application is not optimized for mobile devices yet. Please use a desktop for the best experience.</p>
         </div>
       </div>
-    );
-  return <DesktopDashboardLayout>{children}</DesktopDashboardLayout>;
+      <div className="md:hidden flex h-screen bg-gray-900 items-center justify-center">
+        <DesktopDashboardLayout>{children}</DesktopDashboardLayout>
+      </div>
+    </>
+  );
 }
