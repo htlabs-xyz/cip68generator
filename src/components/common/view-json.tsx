9f5d01e909e4cfa6d464edcc2e4095726c5844a7
tidvn <tiendung0325@gmail.com>
2024-12-06 02:26:48 +0000

    fix: replace JSON rendering with ReactDiffViewer for enhanced diff visualization in ViewMetadataContent

--- src/components/common/view-json.tsx
+++ src/components/common/view-json.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import { JsonValue } from "@prisma/client/runtime/library";
+import ReactDiffViewer from "react-diff-viewer";
 
 export function ViewMetadataContent({ json }: { json: JsonValue }) {
   return (
@@ -8,14 +9,10 @@ export function ViewMetadataContent({ json }: { json: JsonValue }) {
       <DialogTrigger asChild>
         <Button variant="outline">View Content</Button>
       </DialogTrigger>
-      <DialogContent className="sm:max-w-[600px]">
-        <DialogHeader>
-          <DialogTitle></DialogTitle>
-        </DialogHeader>
-        <pre>{JSON.stringify(json, null, 2)}</pre>
-        {/* <DialogFooter>
-          <Button>Close</Button>
-        </DialogFooter> */}
+      <DialogContent className=" max-w-[80vw] w-screen h-[80vh] p-0">
+        <div className="rounded-xl p-10">
+          <ReactDiffViewer oldValue={""} newValue={JSON.stringify(json, null, 2)} splitView={false} useDarkTheme />
+        </div>
       </DialogContent>
     </Dialog>
   );
