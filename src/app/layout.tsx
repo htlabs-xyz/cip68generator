0d8fbbfb641d01f5871aa1cfeb85810fdaf9f3bf
tidvn <tiendung0325@gmail.com>
2024-12-24 21:38:20 +0700

    feat: add favicon and theme color meta tags for improved branding and user experience

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,13 @@ const fontSans = FontSans({
 const RootLayout = function ({ children }: Readonly<PropsWithChildren>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <link rel="apple-touch-icon" sizes="76x76" href={`/images/common/logo.png`} />
+      <link rel="icon" type="image/png" sizes="32x32" href={`/images/common/logo.png`} />
+      <link rel="icon" type="image/png" sizes="16x16" href={`/images/common/logo.png`} />
+      <link rel="manifest" href={`/favicons/site.webmanifest`} />
+      <link rel="mask-icon" href={`/images/common/logo.png`} color="#5bbad5" />
+      <meta name="msapplication-TileColor" content="#000000" />
+      <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
       <body className={cn(fontSans.className)}>{children}</body>
     </html>
   );
