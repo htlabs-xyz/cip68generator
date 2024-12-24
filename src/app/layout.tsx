891b36c856bc011f15f0957bb5ea53fa63cec255
tidvn <tiendung0325@gmail.com>
2024-12-24 23:47:16 +0700

    feat: refactor layout to include mobile notice and restore AppProviders for session management

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import "@/styles/globals.css";
 import { cn } from "@/utils";
 import { appConfig } from "@/constants";
 import { PropsWithChildren } from "react";
+import AppProviders from "@/components/providers";
+import { auth } from "@/lib/auth";
 
 export const metadata: Metadata = {
   title: appConfig.title,
@@ -15,7 +17,8 @@ const fontSans = FontSans({
   weight: ["400", "500", "700"],
 });
 
-const RootLayout = function ({ children }: Readonly<PropsWithChildren>) {
+const RootLayout = async function ({ children }: Readonly<PropsWithChildren>) {
+  const session = await auth();
   return (
     <html lang="en" suppressHydrationWarning>
       <link rel="apple-touch-icon" sizes="76x76" href={`/images/common/logo.png`} />
@@ -25,7 +28,17 @@ const RootLayout = function ({ children }: Readonly<PropsWithChildren>) {
       <link rel="mask-icon" href={`/images/common/logo.png`} color="#5bbad5" />
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
-      <body className={cn(fontSans.className)}>{children}</body>
+      <body className={cn(fontSans.className)}>
+        <div className="lg:hidden h-screen flex items-center justify-center">
+          <div className="text-white text-center py-6 px-8 max-w-md w-full">
+            <h2 className="text-2xl font-semibold mb-4">Important Notice!</h2>
+            <p className="text-lg">The application is not optimized for mobile devices yet. Please use a desktop for the best experience.</p>
+          </div>
+        </div>
+        <div className="hidden lg:block">
+          <AppProviders session={session}>{children}</AppProviders>
+        </div>
+      </body>
     </html>
   );
 };
