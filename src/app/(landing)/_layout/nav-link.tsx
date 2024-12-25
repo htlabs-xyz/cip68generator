7e39cb67ee892cadb2a8077bdef2de015651e042
tidvn <tiendung0325@gmail.com>
2024-12-25 23:19:55 +0700

    feat: enhance login page with wallet connection and redirect functionality

--- src/app/(landing)/_layout/nav-link.tsx
+++ src/app/(landing)/_layout/nav-link.tsx
@@ -1,6 +1,7 @@
+"use client";
 import { cn } from "@/utils";
 import { Button } from "@/components/ui/button";
-import { redirect as navRedirect } from "next/navigation";
+import Link from "next/link";
 type Props = {
   redirect: string;
   name: string;
@@ -12,17 +13,18 @@ type Props = {
 export default function NavLink({ redirect, name, isActive, className }: Props) {
   return (
     <li className="">
-      <Button
-        variant="ghost"
-        onClick={() => navRedirect(redirect)}
-        className={cn(
-          'relative inline-block px-0 py-1 text-[16px] font-normal text-gray-300 before:absolute before:bottom-0 before:left-0 before:h-[1px] before:w-full before:origin-center before:scale-0 before:bg-slate-100 before:transition-all before:duration-300 before:content-[""] hover:before:scale-100',
-          { "before:scale-100": isActive },
-          className,
-        )}
-      >
-        {name}
-      </Button>
+      <Link href={redirect} target={redirect.startsWith("http") ? "_blank" : "_self"}>
+        <Button
+          variant="ghost"
+          className={cn(
+            'relative inline-block px-0 py-1 text-[16px] font-normal text-gray-300 before:absolute before:bottom-0 before:left-0 before:h-[1px] before:w-full before:origin-center before:scale-0 before:bg-slate-100 before:transition-all before:duration-300 before:content-[""] hover:before:scale-100',
+            { "before:scale-100": isActive },
+            className,
+          )}
+        >
+          {name}
+        </Button>
+      </Link>
     </li>
   );
 }
