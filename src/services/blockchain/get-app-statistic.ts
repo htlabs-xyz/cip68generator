30989a5d9d7e8b97ee728cd9e116a8a9537d23b8
Nguyễn Khánh <108068667+independenceee@users.noreply.github.com>
2025-01-06 17:43:37 +0700

    fix: statictis with stake address and match address payment to stake implements detect and fetch data

--- src/services/blockchain/get-app-statistic.ts
+++ src/services/blockchain/get-app-statistic.ts
@@ -1,25 +1,22 @@
 "use server";
 
-import { Cip68Contract } from "@/contract";
 import { blockfrostFetcher } from "@/lib/cardano";
 import { parseError } from "@/utils/error/parse-error";
+import { APP_WALLET_ADDRESS } from "../../../contract/script/constants";
 
 export const getAppStatistic = async () => {
   try {
-    const storedAddress = new Cip68Contract({}).storeAddress;
-    console.log(storedAddress)
-    const response = await blockfrostFetcher.fetchAddressDetail(storedAddress);
-    const storeUtxos = await blockfrostFetcher.fetchUtxoByAddress(storedAddress);
-    const totalUpdate = storeUtxos.length;
-
-    const totalTransaction = response.tx_count;
-    const totalMint = response.received_sum.length - 1;
-    const totalBurn = response.received_sum.length - response.sent_sum.length;
+    const { stake_address } = await blockfrostFetcher.fetchSpecicalAddress(APP_WALLET_ADDRESS);
+    const addresses = await blockfrostFetcher.fetchAccountAssociate(stake_address);
+    const accounts = await blockfrostFetcher.fetchDetailsAccount(stake_address);
+    const totalTransaction = accounts.tx_count;
+    const totalMint = accounts.received_sum.length - 1;
+    const totalBurn = accounts.received_sum.length - accounts.sent_sum.length;
     const data = {
       transaction: totalTransaction,
       mint: totalMint,
       burn: totalBurn,
-      update: totalUpdate,
+      update: addresses.length,
     };
 
     return {
