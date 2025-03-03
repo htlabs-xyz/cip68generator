b841255a856af5afae49e43eaf8d602cd1861f92
tidvn <tiendung0325@gmail.com>
2025-03-03 10:11:14 +0000

    feat: refactor layout components to include AppProviders and session management

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,13 +4,12 @@ import "@/styles/globals.css";
 import { cn } from "@/utils";
 import { PropsWithChildren } from "react";
 import { siteMetadata } from "@/constants/site-metadata";
+import AppProviders from "@/components/providers";
+import { auth } from "@/lib/auth";
 
 export const metadata: Metadata = {
   metadataBase: new URL(siteMetadata.siteUrl),
-  title: {
-    default: siteMetadata.title,
-    template: `%s | ${siteMetadata.title}`,
-  },
+  title: { default: siteMetadata.title, template: `%s | ${siteMetadata.title}` },
   description: siteMetadata.description,
   openGraph: {
     title: siteMetadata.title,
@@ -21,35 +20,18 @@ export const metadata: Metadata = {
     locale: "en_US",
     type: "website",
   },
-  alternates: {
-    canonical: "./",
-    types: {
-      "application/rss+xml": `${siteMetadata.siteUrl}/feed.xml`,
-    },
-  },
+  alternates: { canonical: "./", types: { "application/rss+xml": `${siteMetadata.siteUrl}/feed.xml` } },
   robots: {
     index: true,
     follow: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      "max-video-preview": -1,
-      "max-image-preview": "large",
-      "max-snippet": -1,
-    },
+    googleBot: { index: true, follow: true, "max-video-preview": -1, "max-image-preview": "large", "max-snippet": -1 },
   },
-  twitter: {
-    title: siteMetadata.title,
-    card: "summary_large_image",
-    images: [siteMetadata.socialBanner],
-  },
+  twitter: { title: siteMetadata.title, card: "summary_large_image", images: [siteMetadata.socialBanner] },
 };
-const fontSans = FontSans({
-  subsets: ["latin"],
-  weight: ["400", "500", "700"],
-});
+const fontSans = FontSans({ subsets: ["latin"], weight: ["400", "500", "700"] });
 
 const RootLayout = async function ({ children }: Readonly<PropsWithChildren>) {
+  const session = await auth();
   return (
     <html lang="en" suppressHydrationWarning>
       <link rel="apple-touch-icon" sizes="76x76" href={`/images/common/logo.png`} />
@@ -59,7 +41,9 @@ const RootLayout = async function ({ children }: Readonly<PropsWithChildren>) {
       <link rel="mask-icon" href={`/images/common/logo.png`} color="#5bbad5" />
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
-      <body className={cn(fontSans.className)}>{children}</body>
+      <body className={cn(fontSans.className)}>
+        <AppProviders session={session}>{children}</AppProviders>
+      </body>
     </html>
   );
 };
